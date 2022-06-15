93b33f0d337ced66b311ca239de814c28ff775a3
nguyen.danimal@gmail.com <nguyen.danimal@gmail.com>
2022-06-15 10:24:55 -0500

    problem solving approach 2

--- colt-steele/algorithms/problem-solving-approach.js
+++ colt-steele/algorithms/problem-solving-approach.js
@@ -75,22 +75,78 @@ function charCount(str) {
 // Same Example 2
 // Write a function which takes in a string and returns counts of each character in the string
 
+// function charCount(str) {
+//     // make object to return at end
+//     let result = {}
+//     // loop over string, for each character...
+//     for (let i = 0; i < str.length; i++){
+//         let char = str[i]
+//         // if the char is a number/letter AND a key in object, add one to count
+//         if(result[char] > 0) {
+//             result[char]++;
+//             // if the char is a number/letter AND not in object, add it to object and set value to 1
+//         }   else {
+//             result[char] = 1;
+//         }
+//     }
+//     // return object at end
+//     return result;
+// }
+
+// console.log(charCount("hello"))
+
+/*
+Look Back and Refactor
+Can you check the result?
+Can you derive the result differently?
+Can you understand it at a glance?
+Can you use the result or method for some other problem?
+Can you improve the performance of your solution?
+How have other people solved this problem?
+
+*/
+
+// Same Example - Let's take a look back at the same example and see if we can make it better
+
+// function charCount(str) {
+//     let obj = {}
+//     for (let i = 0; i < str.length; i++) {
+//         let char = str[i].toLowerCase();
+//         if (/[a-z0-9]/.test(char)) {
+//             if (obj[char] > 0) {
+//                 obj[char]++
+//             }   else {
+//                 obj[char] = 1
+//             }
+//         }
+//     }
+//     return obj;
+// };
+
+// console.log(charCount("Hello 1234"))
+
+// How can we refactor this?
 function charCount(str) {
-    // make object to return at end
-    let result = {}
-    // loop over string, for each character...
-    for (let i = 0; i < str.length; i++){
-        let char = str[i]
-        // if the char is a number/letter AND a key in object, add one to count
-        if(result[char] > 0) {
-            result[char]++;
-            // if the char is a number/letter AND not in object, add it to object and set value to 1
-        }   else {
-            result[char] = 1;
+    let obj = {}
+    for (let char of str) {
+        if (isAlphaNumeric(char)) {
+            char = char.toLowerCase();
+            obj[char] = ++obj[char] || 1
         }
     }
-    // return object at end
-    return result;
-}
+    return obj;
+};
 
-console.log(charCount("hello"))
+// checking each char to see if it's alphanumeric
+function isAlphaNumeric(char) {
+    let code = char.charCodeAt(0)
+    if (!(code > 47 && code < 58) && // numeric (0-9)
+        !(code > 64 && code < 91) && // upper alpha (A-Z)
+        !(code > 96 && code < 123)) { // lower alpha (a-z)
+            return false;
+        }
+        return true;
+};
+
+
+console.log(charCount("Hello 1234"))
