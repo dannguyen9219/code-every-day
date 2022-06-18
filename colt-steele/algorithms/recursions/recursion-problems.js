0ccbc9d63163be5a1f309af2574fbb8e155b7083
nguyen.danimal@gmail.com <nguyen.danimal@gmail.com>
2022-06-18 12:36:07 -0500

    colt steele - recursion problem set

--- colt-steele/algorithms/recursions/recursion-problems.js
+++ colt-steele/algorithms/recursions/recursion-problems.js
@@ -35,3 +35,136 @@ fib(4) // 3
 fib(10) // 55
 
 */
+
+/*
+Problem 6
+Write a recrusive function called reverse which accepts a string and treturns a new string in reverse
+reverse('awesome') // 'emosewa'
+
+*/
+
+/*
+Problem 7
+Write a recrusive function called isPalindrome which returns true if the string passed to it is a palindrome.
+isPalindrome('tacocat') // true
+
+*/
+
+/*
+Problem 8
+Write a recursive function called someRecursive which accepts an array and a callback. The function returns true if a single value in the array returns true when passed to the callback. Other it returns false.
+const isOdd = val => val % 2 !== 0
+someRecursive([1,2,3,4], isOdd) // true
+
+*/
+
+/*
+Problem 9
+Write a recursive function called flatten which accepts an array of arrays and returns a new array with all values flattened.
+flatten([1,2,3,[4,5]]) // [1,2,3,4,5]
+
+*/
+
+/*
+Problem 10
+Write a recursive function called capitalizeFirst. Given an arry of strings, capitalize the first letter of each string in the array.
+capitalizeFirst(['car', 'taco', 'banana']) // ['Car', 'Taco', 'Banana']
+
+*/
+
+/*
+Problem 11
+Write a recursive function called nestedEvenSum. Return the sum of all even numbers in an object which may contain nested objects.
+function nestedEvenSum () {
+  // add whatever parameters you deem necessary - good luck!
+}
+
+
+var obj1 = {
+  outer: 2,
+  obj: {
+    inner: 2,
+    otherObj: {
+      superInner: 2,
+      notANumber: true,
+      alsoNotANumber: "yup"
+    }
+  }
+}
+
+var obj2 = {
+  a: 2,
+  b: {b: 2, bb: {b: 3, bb: {b: 2}}},
+  c: {c: {c: 2}, cc: 'ball', ccc: 5},
+  d: 1,
+  e: {e: {e: 2}, ee: 'car'}
+};
+
+nestedEvenSum(obj1); // 6
+nestedEvenSum(obj2); // 10
+
+*/
+
+/*
+Problem 12
+Write a recursive function called capitalizeWords. Given an array of words, return a new array containing each word capitalized.
+// let words = ['i', 'am', 'learning', 'recursion'];
+// capitalizedWords(words); // ['I', 'AM', 'LEARNING', 'RECURSION']
+
+*/
+
+/*
+Problem 13
+Write a function called stringifyNumbers which takes in an object and finds all of the values which are numbers and converts them to strings.
+let obj = {
+    num: 1,
+    test: [],
+    data: {
+        val: 4,
+        info: {
+            isRight: true,
+            random: 66
+        }
+    }
+}
+
+
+stringifyNumbers(obj)
+
+
+{
+    num: "1",
+    test: [],
+    data: {
+        val: "4",
+        info: {
+            isRight: true,
+            random: "66"
+        }
+    }
+}
+
+*/
+
+/*
+Problem 14
+Write a function called collectStrings which accepts an object and returns an array of all the values in the object that have a typeof string.
+const obj = {
+    stuff: "foo",
+    data: {
+        val: {
+            thing: {
+                info: "bar",
+                moreInfo: {
+                    evenMoreInfo: {
+                        weMadeIt: "baz"
+                    }
+                }
+            }
+        }
+    }
+}
+
+collectStrings(obj) // ["foo", "bar", "baz"])
+
+*/
